ad996b95322fcf905c37c94d79485f84d2d3a468
Evan Weinberg <Evan@Amys-MacBook-Air-2.local>
2018-12-18 20:30:19 -0500

    Added shirtselection for all areas

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class ShirtSection extends Component {
       }}>
         {selected ? <input type="file" id="files" onChange={this.fileSelected} className="upload-button" /> : ''}
             
-        {imgUrl ? <img id="image" src={imgUrl} style={{width:width}} /> : null}
+        {imgUrl ? <img id="image" src={imgUrl} style={{maxWidth:'100%'}} /> : null}
           
       </div>
     )
@@ -89,14 +89,33 @@ class App extends Component {
 
   render() {
     const {selectedSection, showTemplate} = this.state
-    const selectedId = selectedSection && selectedSection.id 
+    const selectedId = selectedSection && selectedSection.id
+    let areas = []
+    for(let i = 0; i < 8; i++) {
+      let offset = i > 3 ? 44 : 19; 
+      areas.push(<ShirtSection key={`section_${i}`} selected={selectedId === `SECTION_${i}`} id={`SECTION_${i}`} onClick={this.selectArea} left={66*i+offset} top={355} width={64} height={128} />
+      )
+    }
+
+    areas = areas.concat([
+      <ShirtSection selected={selectedId === "DOWN"} id="DOWN" onClick={this.selectArea} left={231} top={204} width={128} height={64} />,
+      <ShirtSection selected={selectedId === "UP"} id="UP" onClick={this.selectArea} left={231} top={8} width={128} height={64} />,
+          
+      <ShirtSection selected={selectedId === "FRONT"} id="FRONT" onClick={this.selectArea} left={231} top={74} width={128} height={128} />,
+      <ShirtSection selected={selectedId === "BACK"} id="BACK" onClick={this.selectArea} left={427} top={74} width={128} height={128} />,
+          
+      <ShirtSection selected={selectedId === "R"} id="R" onClick={this.selectArea} left={165} top={74} width={64} height={128} />,
+      <ShirtSection selected={selectedId === "L"} id="L" onClick={this.selectArea} left={361} top={74} width={64} height={128} />
+    ])
+
+
     return (
       <div className="App">
         <div className="image-container">
           <img src={template} id="templateImage" onClick={this.clearSelection} className={!showTemplate ? 'hidden' : ''} />
-          <ShirtSection selected={selectedId === "1"} id="1" onClick={this.selectArea} left={231} top={73} width={128} height={130} />
-          <ShirtSection selected={selectedId === "2"} id="2" onClick={this.selectArea} left={231} top={8} width={128} height={65} />
+          {areas}
         </div>
+
         <button className="hide-show" onClick={this.downloadImage}>Download Image</button>
         
       </div>
